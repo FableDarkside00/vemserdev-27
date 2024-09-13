aa7f2bbec903f026de3679530e2e21c85f9064ea
Saymon Davi De Macedo <126501494+fable00@users.noreply.github.com>
2024-09-13 16:05:20 -0300

    avaliacao.c

--- avaliacao.c
+++ avaliacao.c
@@ -3,18 +3,39 @@
 
 int* calcPessoa()
 {
-    int *erro = -1;
+    int *list_pessoa = (int*)malloc(3*sizeof(int));
+    if (list_pessoa == NULL) {
+        printf("Erro ao alocar memoria");
+        exit(1);
+    }
+    
     int num_pessoa;
     int num_homem;
     int num_mulher;
+
     printf("\nQuantas pessoas irao a festa?(8 - 20): ");
     scanf("%d", &num_pessoa);
+    
     //Check minimal quantity
-    if (num_pessoa < 8 || num_pessoa > 20) return erro;
-    printf("\nQuantos são homens?: ");
+    if (num_pessoa < 8 || num_pessoa > 20){
+        free(list_pessoa);
+        return NULL;
+    
+    };
+    printf("\nQuantos sao homens?: ");
     scanf("%d", &num_homem);
+    
+    if (num_homem > num_pessoa)
+    {
+        free(list_pessoa);
+        return NULL;
+    }
+
     num_mulher = num_pessoa - num_homem;
-    int list_pessoa[3] = {num_pessoa, num_homem, num_mulher};
+    list_pessoa[0] = num_pessoa;
+    list_pessoa[1] = num_homem;
+    list_pessoa[2] = num_mulher;
+    
     return list_pessoa;
 }
 
@@ -35,7 +56,7 @@ void calcCarne(int* num_pessoa)
     int qnt_cerveja_h = num_pessoa[1] * 4;
     int qnt_cerveja_m = num_pessoa[2] * 2;
 
-    int* list_qnt[7] = {&qnt_carne_h, &qnt_carne_m, &qnt_queijo_h, &qnt_queijo_m, &qnt_linguica, &qnt_cerveja_h, &qnt_cerveja_m};
+    int list_qnt[7] = {qnt_carne_h, qnt_carne_m, qnt_queijo_h, qnt_queijo_m, qnt_linguica, qnt_cerveja_h, qnt_cerveja_m};
     int size_qnt = (sizeof(list_qnt) / sizeof(list_qnt[0]));
     printf("\nA quantidade respectiva para homens e mulheres de carne, queijo, linguica e cerveja e:\n");
     for (int i = 0; i < size_qnt; i++)
@@ -46,13 +67,20 @@ void calcCarne(int* num_pessoa)
 
 int main()
 {
-    system("clear");
     printf("\nBem vindo ao sistema de organizacao de churrascos!");
+    printf("\nMede a quantidade respectiva para homens e mulheres de carne, queijo, linguica e cerveja em gramas e por numero de garrafas\n");
     int* num_pessoa = calcPessoa();
-    if (num_pessoa[0] == -1) {
+    int question;
+    if (num_pessoa == NULL) {
         printf("\nQuantidade invalida!");
         return main();
     }
     calcCarne(num_pessoa);
+    free(num_pessoa);
+
+    printf("\nDeseja repetir?(1 - Repetir | 0 - Sair): ");
+    scanf("%d", &question);
+    if (question == 1) return main();
+    exit(0);
     return 0;
 }
